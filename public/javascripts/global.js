79c6228eab6a172c7fbf1b3c3ad680d98cc49d44
Justin Nguyen <jnguy876@gmail.com>
2017-12-30 15:56:08 -0800

    added to home page

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -3,16 +3,62 @@ var currentUserId = '';
 
 $(document).ready(function(){
 
+	populateTables();
+
 	$('#btnLogin').on('click', loginUser);
 
 	$('#btnRegister').on('click', addUser);
 
 });
 
+function populateTables(){
+
+	var cfcTableContent = '';
+	var svmTableContent = '';
+	var studentBooks = 0;
+	var mikeBooks = 0;
+	var readCFC = [];
+
+	$.getJSON( '/users/userlist', function( data ) {
+
+    	userListData = data;
+        // For each item in our JSON, add a table row and cells to the content string
+        $.each(data, function(){
+        	if(this.username == "mkang"){
+        		mikeBooks = this.books.length;
+        	}
+        	else{
+        		studentBooks += this.books.length;
+        	}
+        	this.books.forEach(function(book){
+        		if(book.title == "Case for Christmas"){
+        			readCFC.push(this.fullname);
+        		}
+        	});
+
+        });
+        svmTableContent += '<tr>';
+        svmTableContent += '<td>'+ studentBooks +'</td>';
+        svmTableContent += '<td>' + mikeBooks + '</td>';
+        svmTableContent += '</tr>';
+        // Inject the whole content string into our existing HTML table
+        $('#svmList table tbody').html(svmTableContent);
+        readCFC.forEach(function(name){
+        	cfcTableContent += '<tr>';
+        	cfcTableContent += '<td>' + name + '</td>';
+        	cfcTableContent += '</tr>';
+        });
+        $('#cfcList table tbody').html(cfcTableContent);
+    });
+
+}
+
 function loginUser(event){
 	event.preventDefault();
 	var userName = $('#login input#loginUserName').val();
 	var pass = $('#login input#loginPassword').val();
+	var success = false;
+	var hasPrompted = false;
 
 	$.getJSON( '/users/userlist', function( data ) {
 		userListData = data;
@@ -21,17 +67,19 @@ function loginUser(event){
 			if(item.username === userName && item.password === pass){
 				currentUserId = item._id;
 				window.location.href = "/home";
+				success = true;
 				return false;
 			}
 			else if(item.username === userName){
 				window.alert("Incorrect Password");
-				return false;
-			}
-			else{
-				window.alert("Incorrect User Name/Password combination");
+				hasPrompted = true;
 				return false;
 			}
 		});
+		if(!success && !hasPrompted){
+			window.alert("Incorrect User Name/Password combination");
+			return false;
+		}
 	});
 };
 
@@ -58,7 +106,8 @@ function addUser(event){
             'password': $('#register input#regPassword').val(),
             'email': $('#register input#regEmail').val(),
             'year': $('#register select#regYear').val(),
-            'gender': $('#register select#regGender').val()
+            'gender': $('#register select#regGender').val(),
+            'books': "[{'title' : 'Case For Christmas', 'author' : 'Lee Strobel'}{'title' : 'Mere Christianity', 'author' : 'C.S. Lewis'}]"
         }
 
         $.ajax({
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var responses_x_questions = require('../reports/barChart.json');  
+var clone = require('clone');
 
 /* GET index page. */
 router.get('/', function(req, res, next) {
@@ -22,6 +24,14 @@ router.get('/home', function(req,res){
 	// for(key in docs){
 	// 	keys += key.username + ' ';
 	// }
+	// var chartOptions = clone(responses_x_questions);
+
+	// var categories = ["newCat1","newCat2","newCat3","newCat4","newCat5"];
+
+	// chartOptions.xAxis[0].data = categories;
+	// chartOptions.series[0].data = [10,20,30,40,50];
+
+	// res.render('home', { title: 'Express', data: JSON.stringify(chartOptions), userlist : collection });
 	res.render('home', { title: 'A2F Winter Reading Callenge', numUsers : 2});
 });
 
