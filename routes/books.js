9c6320707f83bf4d6625efedf15838a8dadc311f
Justin Nguyen <jnguy876@gmail.com>
2018-01-03 15:19:18 -0800

    Buttons from Rec Books to Home and Profile

--- routes/books.js
+++ routes/books.js
@@ -4,8 +4,8 @@ var responses_x_questions = require('../reports/barChart.json');
 var clone = require('clone');
 
 /* GET recommended books page. */
-router.get('/recommendedbooks', function(req, res, next) {
-  res.render('recbooks', { title: 'List of Recommended Books' });
+router.get('/recommendedbooks/:id', function(req, res, next) {
+  res.render('recbooks', { title: 'List of Recommended Books', id: req.params.id});
 });
 
 /* GET books read page. */
